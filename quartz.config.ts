696b06c68bba1ba73105a89e4e81edc048e53f4f
jrekier <jrekier@users.noreply.github.com>
2024-06-09 15:36:25 +0200

    add linked citations

--- quartz.config.ts
+++ quartz.config.ts
@@ -65,7 +65,7 @@ const config: QuartzConfig = {
       }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
-      Plugin.Citations(),
+      Plugin.Citations({ linkCitations: true }),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
